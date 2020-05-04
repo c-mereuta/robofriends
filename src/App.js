293f935b3884121d354c945598b5ce31b7604e0d
Cristian Mereuta <cristian.mereuta@credit-suisse.com>
2020-05-04 14:53:35 +0200

    added loading screen

--- src/App.js
+++ src/App.js
@@ -33,13 +33,17 @@ class App extends Component {
         .toLocaleLowerCase()
         .includes(this.state.searchfield.toLocaleLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f2">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Cardlist robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f2">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Cardlist robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
